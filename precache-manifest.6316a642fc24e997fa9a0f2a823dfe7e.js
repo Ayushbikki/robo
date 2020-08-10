106a3afed9f61a8820bbbae947c4a1f9e326f0f9
Ayushbikki <64982809+Ayushbikki@users.noreply.github.com>
2020-08-10 14:12:48 +0530

    Updates

---
+++ precache-manifest.6316a642fc24e997fa9a0f2a823dfe7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3d618bd14303702ec26c7ba503296b",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2d554c34d8bbf6e67237",
+    "url": "/robo/static/css/main.16b1e22e.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robo/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d554c34d8bbf6e67237",
+    "url": "/robo/static/js/main.3ead1778.chunk.js"
+  },
+  {
+    "revision": "af839f1de40b3e92ec6c",
+    "url": "/robo/static/js/runtime-main.99a06c5d.js"
+  }
+]);
